39b3dd6f4e97b8ad28949343f195da1d9487a022
wteam-xq <857609086@qq.com>
2016-01-22 21:18:36 +0800

    首页添加

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'app/main.js'),
+    entry: path.resolve(__dirname, 'js/main.js'),
     output: {
         path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
+        filename: 'js/main.js',
     }
 };
